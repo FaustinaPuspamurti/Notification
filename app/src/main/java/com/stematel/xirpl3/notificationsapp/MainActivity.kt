0e9439289a05342a37905e3d6e04b5df9111fd10
FaustinaPuspamurti <faustinapuspamurti2105@gmail.com>
2021-05-07 22:13:37 +0700

    KK4A

---
+++ app/src/main/java/com/stematel/xirpl3/notificationsapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.stematel.xirpl3.notificationsapp
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationManager: NotificationManager
+    lateinit var builder: Notification.Builder
+    private val channelId = "12345"
+    private val description = "Hallo,Saya dari kelas XI RPL 3"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                NotificationManager
+    }
+    fun btnNotify(view: View) {
+        val intent = Intent(this, LauncherActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager .IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+            builder = Notification.Builder(this, channelId).setContentTitle("Faustina " +
+                    "Puspamurti").setContentText("Hallo,Saya dari kelas XI RPL 3").setSmallIcon(R.drawable .ic_brightness).setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable
+                    .ic_launcher_background)).setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(12345, builder.build())
+    }
+}
